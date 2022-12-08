3eff4d67a0d5a0dc7827b882f6f319208ba2c58b
josh <JuleeGunjal>
2022-12-08 14:50:13 +0530

    Used Constants instead of patterns in regex

--- StudentAttendancePortal/app/models/teacher.rb
+++ StudentAttendancePortal/app/models/teacher.rb
@@ -1,9 +1,8 @@
 class Teacher < ApplicationRecord
-  validates :email, presence: { message: I18n.t("blank") }, uniqueness: { message: I18n.t("unique") }, format: { with: /\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})/, message: I18n.t("invalid")}
-  validates :password, format: { with: /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/,
-    message: I18n.t("pattern") }
+  validates :email, presence: { message: I18n.t("blank") }, uniqueness: { message: I18n.t("unique") }, format: { with: EMAIL_REGEX, message: I18n.t("invalid")}
+  validates :password, format: { with: PASSWORD_REGEX, message: I18n.t("pattern") }
   validates :password, confirmation: true
-  validates :mobile_no, length: { is: 13, message: I18n.t("wrong_length") }, uniqueness: true, format: { with: /(\+?\d{1,4}[\s-])?(?!0+\s+,?$)\d{10}\s*,?/, message: I18n.t("invalid")}
+  validates :mobile_no, uniqueness: true, format: { with: MOBILE_NO_REGEX, message: I18n.t("invalid")}
 
   after_save :send_welcome_mail
 
